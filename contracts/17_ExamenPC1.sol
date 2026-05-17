684f3cd3dc99a8e1ed855ffb691189c8eca408ea
JeffryRU <jromerou2@upao.edu.pe>
2026-05-16 20:27:20 -0500

    Parte 3

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.12 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "hardhat/console.sol";
 
 contract Biblioteca261784 {
-
     // Contrato desplegado en la Parte 1
     address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
 
@@ -12,28 +11,39 @@ contract Biblioteca261784 {
         uint256 id;
         string titulo;
         string autor;
+        bool estado; // Disponible o no
     }
 
     Libro[] public libros;
 
-    constructor() {
-        string memory idEstudiante = "261784";
-        string memory nombreEstudiante = "Jeffry Anderson Romero Uriol";
-
-        string memory mensaje = string.concat("Ejecutado por: ", idEstudiante, " - ", nombreEstudiante);
-        
-        console.log(mensaje);
+    // Modificador para centralizar el mensaje
+    modifier mensajeEjecucion() {
+        console.log("Ejecutado por: 261784 - Jeffry Anderson Romero Uriol");
+        _;
     }
 
-    // Agregar un elemento
-    function agregarElemento(uint256 _id, string memory _titulo, string memory _autor) public {
-        libros.push(Libro(_id, _titulo, _autor));
+    // Modificador
+    constructor() mensajeEjecucion {}
+
+    function agregarElemento(
+        uint256 _id,
+        string memory _titulo,
+        string memory _autor,
+        bool _estado
+    ) public mensajeEjecucion {
+        // Validación de ID repetido
+        for (uint i = 0; i < libros.length; i++) {
+            require(libros[i].id != _id, "Error: ID repetido");
+        }
+
+        // Validación de atributo de Parte 1 (titulo no vacio)
+        require(bytes(_titulo).length > 0, "Error: Titulo vacio");
+
+        libros.push(Libro(_id, _titulo, _autor, _estado));
     }
 
     // Contar elementos
-    function contarElementos() public view returns (uint256) {
-        console.log("Ejecutado por: 261784 - Jeffry Anderson Romero Uriol");
-        
+    function contarElementos() public view mensajeEjecucion returns (uint256) {
         return libros.length;
     }
 }
