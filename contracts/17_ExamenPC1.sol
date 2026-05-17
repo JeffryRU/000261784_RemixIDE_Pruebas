395cb958beb0afe63524bc16e692e95c12525d7a
JeffryRU <jromerou2@upao.edu.pe>
2026-05-16 20:13:55 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity >=0.8.12 <0.9.0;
 
 import "hardhat/console.sol";
 
 contract Biblioteca261784 {
+
+    // Contrato desplegado en la Parte 1
+    address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
 
     struct Libro {
         uint256 id;
@@ -20,5 +23,17 @@ contract Biblioteca261784 {
         string memory mensaje = string.concat("Ejecutado por: ", idEstudiante, " - ", nombreEstudiante);
         
         console.log(mensaje);
+    }
+
+    // Agregar un elemento
+    function agregarElemento(uint256 _id, string memory _titulo, string memory _autor) public {
+        libros.push(Libro(_id, _titulo, _autor));
+    }
+
+    // Contar elementos
+    function contarElementos() public view returns (uint256) {
+        console.log("Ejecutado por: 261784 - Jeffry Anderson Romero Uriol");
+        
+        return libros.length;
     }
 }
